37e8276a09f218c7252367bb6c180a38908d6fce
Johnson <jo74705@gmail.com>
2020-07-25 16:08:15 +0800

    init app

---
+++ android/app/src/main/java/com/example/rn_integration_with_existing_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rn_integration_with_existing_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
